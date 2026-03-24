85617a13a16d02380e2a715e9939c551a16d67af
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-03-24 15:02:03 +0100

    Add middlewares to app

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,8 @@ app.use(customLogger());
 app.use(morgan('dev'));
 app.use(cors({ origin: '*' }));
 app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static('./public'));
 
 app.get('/', (_req, res) => {
     res.send('Hello World!');
@@ -26,7 +28,10 @@ app.get('/patata', (_req, _res, next) => {
     return;
 });
 
-app.post('/', (_req, res) => {
+app.post('/', (req, res) => {
+    log(req.body);
+    log(req.params);
+    log(req.query);
     res.statusCode = 201;
     res.send('Hello POST');
     return;
@@ -34,6 +39,12 @@ app.post('/', (_req, res) => {
 
 app.get('/api', (_req, res) => {
     res.send('API Rest');
+    return;
+});
+
+app.get('/api/notes', (_req, res) => {
+    const notes = [{ id: 1 }, { id: 2 }];
+    res.json(notes);
     return;
 });
 
